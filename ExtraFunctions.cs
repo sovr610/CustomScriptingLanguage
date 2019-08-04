292cf060ab5bd0d0191018132b1ef6e6b057e320
parker bidigare <work.simpleintelligence@gmail.com>
2019-08-03 20:49:45 -0400

    init commit, make sure to subscribe to PBCoder on youtube

---
+++ ExtraFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace liveStreamOne
+{
+    public class ExtraFunctions
+    {
+        public void add(double a, double b)
+        {
+            var c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NLua;
+
+
+namespace liveStreamOne
+{
+    class Program
+    {
+        private Lua lua = new Lua();
+
+        static void Main(string[] args)
+        {
+            Program pgrm = new Program();
+            ExtraFunctions extra = new ExtraFunctions();
+            pgrm.addFunction("printOnConsole", pgrm);
+            pgrm.addFunction("add", extra);
+            Console.WriteLine("-- PBCode Script --");
+            while (true)
+            {
+                Console.Write(":>");
+                string cmd = Console.ReadLine();
+                if(!String.IsNullOrEmpty(cmd))
+                {
+                    pgrm.runScript(cmd);
+                }
+            }
+        }
+
+        public void runScript(string cmd)
+        {
+            try
+            {
+                lua.DoString(cmd);
+            }
+            catch(Exception i)
+            {
+                Console.WriteLine(i.Message);
+            }
+        }
+
+        public void printOnConsole(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public bool addFunction(string functionName, object classType)
+        {
+            try
+            {
+                Type type = classType.GetType();
+                MethodInfo[] MI = type.GetMethods();
+                IEnumerable<MethodInfo> linq = from a in MI
+                                               where a.Name == functionName
+                                               select a;
+                if (linq.Count() > 0) {
+                    MethodInfo info = linq.ToList()[0];
+                    lua.RegisterFunction(functionName, classType, info);
+                    return true;
+                }
+
+                return false;
+            }
+            catch(Exception i)
+            {
+                Console.WriteLine(i);
+                return false;
+            }
+
+        }
+
+    }
+}
